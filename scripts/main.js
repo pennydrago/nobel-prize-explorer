b695a0989944e5f887ef20c8f5d1c3c04781d0b2
Penelope Drago <prd@drago.com.au>
2023-04-24 13:18:04 +1000

    add light and dark mode

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,23 @@
 // Code to test connection to HTML file
 console.log('Hello');
+
+// Function to create light/dark mode toggle
+
+let modeButton = document.getElementById('dark-mode-button');
+
+modeButton.addEventListener('click', function () {
+
+  document.body.classList.toggle('dark-mode');
+
+  let darkElements = document.querySelectorAll('.dark-element');
+  darkElements.forEach(function (darkElement) {
+    darkElement.classList.toggle('dark-mode-element')
+  });
+
+  let darkCards = document.querySelectorAll('.dark-card');
+  darkCards.forEach(function (darkCard) {
+    darkCard.classList.toggle('dark-mode-card')
+  });
+
+});
+
